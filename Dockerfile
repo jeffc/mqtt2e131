39b1c83676e1f7e463297723ab4798c67a3b83e1
Jeff Cooper <jeff@thejeffcooper.com>
2020-12-03 01:29:40 -0500

    dockerfile updates

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
-COPY *.py ./
+COPY mqtt2e131.py ./
 
 CMD ["python", "run.py"]
--- example_usage.py
+++ example_usage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# If you're using docker, name this file run.py
+# If you're using docker with the provided Dockerfile,
+# name this file run.py and COPY or bind-mount it as /code/run.py
+# (I use bind mounting for easy tweaks, but YMMV)
 from mqtt2e131 import *
 import time
 
@@ -9,8 +11,12 @@ HOST = "hostname_or_ip"
 NUM_UNIVERSES_ON_HOST = 3
 NUM_LIGHTS = 381
 
-target = SACNTarget(HOST, NUM_UNIVERSES_ON_HOST)
-L = Light("light_name_in_hass", target, MQTT_SERVER, 1, NUM_LIGHTS, color_order=Light.ORDER_RGB)
+def main():
+  target = SACNTarget(HOST, NUM_UNIVERSES_ON_HOST)
+  L = Light("light_name_in_hass", target, MQTT_SERVER, 1, NUM_LIGHTS, color_order=Light.ORDER_RGB)
 
-while True:
-  time.sleep(1)
+  while True:
+    time.sleep(1)
+
+if __name__ == "__main__":
+  main()
